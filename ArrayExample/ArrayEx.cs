f1afae187a17b68e6c941d125d7c9100d03044cf
Ahmed <ahmed.ismail14141414@gmail.com>
2024-01-20 08:59:38 +0200

    Add project files.

---
+++ ArrayExample/ArrayEx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExample
+{
+    internal class ArrayEx
+    {
+        public void arrayBuild (int num)
+        {
+            String[] arrayList = new string[num];
+            for(int i = 0; i < num; i++)
+            {
+                Console.WriteLine("Please enter the client number " + (i+1));
+                arrayList[i] = Console.ReadLine();
+            }
+            for(int x = 0; x < arrayList.Length; x++)
+            {
+                Console.WriteLine(" the name Client number  " + ( x+1 ) + " : "+ arrayList[x]);
+            }
+        }
+       
+    }
+}
---
+++ ArrayExample/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ArrayExample;
+ArrayEx arr = new ArrayEx();
+Console.WriteLine(" please enter Count  of Clients");
+try
+{
+    String s = Console.ReadLine();
+    int count = Convert.ToInt32(s);
+    arr.arrayBuild(count);
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
